96f7dba11640d6500b78e88a35bb4c997d7cb277
Кирилл Кириленко <k.kirilenko@lucky.online>
2020-07-10 20:46:43 +0300

    Initial Commit

---
+++ SkyengTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkyengTest
+//
+//  Created by godemodegame on 10.07.2020.
+//  Copyright © 2020 godemodegame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
